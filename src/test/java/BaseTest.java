37c2ff248e7156a8a0e7a23c750c6a592f064cbb
Alexandra Kunitskaya <alexandra.kunitskaya@gmail.com>
2018-06-24 22:54:59 +0300

    Add descriptions

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,28 @@
+package FirstSuite;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+    @BeforeSuite
+    public void setUpBeforeSuite(){
+        System.out.println("Set up beforeSuite");
+    }
+    @BeforeClass
+    public void setUp() {
+        System.out.println("Set up from BaseTest, test executed: " + getClass().getName());
+    }
+
+    @AfterClass
+    public void tearDown(){
+        System.out.println("Tear down from BaseTest, test finished: " + getClass().getName());
+    }
+
+    @AfterSuite
+    public void tearDownAfterSuite(){
+        System.out.println("Tear down after suite");
+    }
+}
---
+++ src/test/java/FirstSuite/SubTest.java
@@ -0,0 +1,30 @@
+package FirstSuite;
+
+import com.epam.tat.module4.Calculator;
+import com.sun.org.glassfish.gmbal.Description;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * In this class the test should pass twice
+ * Parameters are taken from dataProvider
+ */
+public class SubTest extends FirstSuite.BaseTest{
+    Calculator calculator = new Calculator();
+
+    @Test(dataProvider="subDataProvider", priority = 4)
+    @Description("validate substraction with doubles")
+    public void validateSub(double a, double b, double expectedResult){
+        double actualResult = calculator.sub(a, b);
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @DataProvider(name = "subDataProvider")
+    public Object[][] dataProvider(){
+        return new Object[][]{
+                {100, 10, 90},
+                {200, 20, 180}};
+    }
+    }
