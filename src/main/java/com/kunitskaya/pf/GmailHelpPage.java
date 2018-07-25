bb67ad4d97b4bfe40e4c60f161e2c85a2a3e2d88
Alexandra Kunitskaya <alexandra.kunitskaya@gmail.com>
2018-07-25 18:27:30 +0300

    Change locators for new help page

--- src/main/java/com/kunitskaya/pf/GmailHelpPage.java
+++ src/main/java/com/kunitskaya/pf/GmailHelpPage.java
@@ -4,14 +4,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class GmailHelpPage extends AbstractPage {
-    public static final String HELP_PAGE_HEADER = "Welcome to the Gmail Help Center";
+    public static final String HELP_PAGE_HEADER = "How can we help you?";
     public static final String HELP_PAGE_TITLE = "Gmail Help";
     public static final String HELP_PAGE_SEARCH_PLACEHOLDER = "Describe your issue";
 
-    @FindBy(css = "nav.accordion-homepage h1")
+    @FindBy(xpath = "//h1/div/following-sibling::div")
     WebElement header;
 
-    @FindBy(xpath = "//input[@class = 'gb_5e' and @name='q']")
+    @FindBy(xpath = "//input[@class = 'promoted-search__input' and @name='q']")
     WebElement searchField;
 
     public String getHeader() {
