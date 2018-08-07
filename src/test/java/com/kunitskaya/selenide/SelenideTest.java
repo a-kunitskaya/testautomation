61599fd9cfacc053381e123f7e442349ff602784
Alexandra <Alexandra.kunitskaya@gmail.com>
2018-08-07 10:43:37 +0300

    Add selenide test

--- src/test/java/com/kunitskaya/selenide/SelenideTest.java
+++ src/test/java/com/kunitskaya/selenide/SelenideTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.kunitskaya.pages.LoginPage.LOGIN_PAGE_URL;
@@ -20,13 +21,30 @@ public class SelenideTest {
     @Test
     public void logIn() {
         User user = TestDataProvider.getUser();
-        open(LOGIN_PAGE_URL);
-        $(By.id("identifierId"))
-                .setValue(user.getUsername())
-                .pressEnter();
+        String username = user.getUsername();
+        String password = user.getPassword();
 
-        $(By.name("password"))
-                .setValue(user.getPassword())
-                .pressEnter();
+        open(LOGIN_PAGE_URL);
+
+        //enter username, proceed
+        $(By.id("identifierId")).setValue(username)
+                                .pressEnter();
+
+        //assert that the entered username == user username
+        $(By.id("profileIdentifier")).shouldHave(text(username));
+
+        //enter password, proceed
+        $(By.name("password")).setValue(password)
+                              .pressEnter();
+
+        //assert that the entered password == user password
+
     }
 }
+
+
+//        String actualPageAfterLogin = webDriver.getCurrentUrl();
+//        assertEquals(actualPageAfterLogin, INBOX);
+//
+//        WebElement loggedInAccountButton = webDriver.findElement(By.cssSelector(".gb_b.gb_db.gb_R"));
+//        assertTrue(loggedInAccountButton.isDisplayed());
