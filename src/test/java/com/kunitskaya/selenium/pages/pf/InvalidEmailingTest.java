6d5871408cafa4baa8a5e830cd8980183a80fc6e
Alexandra_Kunitskaya <alexandra_kunitskaya@epam.com>
2018-09-18 13:22:49 +0300

    Change naming

--- src/test/java/com/kunitskaya/selenium/pages/pf/InvalidEmailingTest.java
+++ src/test/java/com/kunitskaya/selenium/pages/pf/InvalidEmailingTest.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class InvalidEmailTest extends SeleniumBaseTest {
+public class InvalidEmailingTest extends SeleniumBaseTest {
 
     @BeforeClass
     public void logIn() {
